e774d55a8629e49e88e51dcc6fd86f15bc52d7c3
Ajay Prasadh Viswanathan <ajay@Ajays-MacBook-Pro.local>
2016-10-21 16:39:30 +0100

    did feature extraction through bag of words

--- author_identification/__main__.py
+++ author_identification/__main__.py
@@ -1,2 +1,36 @@
 from ingestor import *
-data = ingest()
+import re
+from feature_extractor import *
+import numpy as np
+import os
+from sklearn.cross_validation import train_test_split
+
+def prepare_X_y(data_path):
+	'''
+		READ RAW DATA, COMPUTE FEATURES AND SAVES X and y to numpy array locally
+	'''
+	data = ingest()
+	list_features = compute_list_features(data)
+	print len(list_features)
+	X,y = convert_data_features(data,list_features)
+	np.save(data_path + 'X.npy')
+	np.save(data_path + 'y.npy')
+
+
+
+cwd = os.getcwd()
+data_path = cwd + '/author_identification/data/'
+#prepare_X_y(data_path)
+
+X = np.load(data_path + 'X.npy')
+y = np.load(data_path + 'y.npy')
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.25, random_state=42)
+X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5, random_state=42)
+
+
+
+
+
+
+
---
+++ author_identification/feature_extractor.py
@@ -0,0 +1,48 @@
+import re
+from collections import Counter
+import numpy as np
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelBinarizer
+
+def compute_list_features(data):
+	'''
+		IDENTIFIES WORDS WITH FREQUENCY >=5 ACROSS DOCUMENTS AND RETURNS THE WORDS
+	'''
+	word_str = ''
+	targets = data.keys()
+	for target in targets:
+		word_str = word_str + ' '.join(data[target])
+	
+	word_str = re.sub(r'[^\w\s]','',word_str)	
+	word_list = word_str.split(' ')
+	
+	all_features = Counter(word_list)
+	filtered_features = dict(filter(lambda x:x[1]>=50, all_features.items())).keys()
+	return filtered_features
+
+def convert_data_features(data,list_features):
+	'''
+		GIVEN DATA DICTIONARY AND LIST OF FEATURES, COMPUTE FEATURE VECTOR FOR EACH ROW IN DATA 
+	'''
+	X = []
+	y = []
+	enc = LabelBinarizer()
+	print data.keys()
+	enc.fit(data.keys())
+	for target in data.keys():
+		print target
+		for sentence in data[target]:
+			vec = np.zeros((1,len(list_features)))	
+			for i in range(len(list_features)):
+				vec[0,i] = data[target].count(list_features[i])
+			X.append(vec)
+			y.append(enc.transform([target]))
+	X = np.array(X)
+	X = X.reshape((X.shape[0],X.shape[2]))
+	y = np.array(y)
+	y = y.reshape((y.shape[0],y.shape[2]))
+		
+	return X, y
+	
+
+	
